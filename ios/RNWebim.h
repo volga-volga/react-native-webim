517109f27556105f40183727c3ad3e0fd002288b
AlfClausen <alf_clausen@mail.ru>
2019-10-10 10:46:35 +0300

    Fix swift imports at RNWEBIM.h

--- ios/RNWebim.h
+++ ios/RNWebim.h
@@ -6,8 +6,23 @@
 #endif
 
 #import <React/RCTEventEmitter.h>
-#import "RNWebim-Swift.h"
 
-@interface webim : RCTEventEmitter <UIImagePickerControllerDelegate, UINavigationControllerDelegate, RCTBridgeModule, MessageListener, SendFileCompletionHandler, RateOperatorCompletionHandler>
+@class MessageListener;
+@class SendFileCompletionHandler;
+@class RateOperatorCompletionHandler;
+@protocol UIImagePickerControllerDelegate;
+@protocol UINavigationControllerDelegate;
+@protocol RCTBridgeModule;
 
+@interface MyObjcClass : RCTEventEmitter
+- (MessageListener *)returnSwiftClassInstance;
+- (SendFileCompletionHandler *)returnSwiftClassInstance2;
+- (RateOperatorCompletionHandler *)returnSwiftClassInstance3;
+- (id <UIImagePickerControllerDelegate, UINavigationControllerDelegate, RCTBridgeModule>)returnInstanceAdoptingSwiftProtocol;
 @end
+
+//#import "RNWebim-Swift.h"
+//
+//@interface webim : RCTEventEmitter <UIImagePickerControllerDelegate, UINavigationControllerDelegate, RCTBridgeModule, MessageListener, SendFileCompletionHandler, RateOperatorCompletionHandler>
+//
+//@end
