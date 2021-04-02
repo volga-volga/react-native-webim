81626848cdfa138937a0cfa75f54a7568faf8aa0
Maxim Vasin <maxim.vasin@mcc-tomsk.de>
2021-04-02 17:23:34 +0700

    Upd

--- ios/RNWebim.m
+++ ios/RNWebim.m
@@ -101,11 +101,6 @@ RCT_EXPORT_METHOD(resumeSession:(NSDictionary*) builderData reject:(RCTResponseS
     resolve(@[@{}]);
 }
 
-RCT_EXPORT_METHOD(getUnreadMessagesCounter:(RCTResponseSenderBlock) reject resolve:(RCTResponseSenderBlock) resolve) {
-    NSError *err = nil;
-    retrun 0;
-}
-
 RCT_EXPORT_METHOD(destroySession:(NSNumber*) clearUserData reject:(RCTResponseSenderBlock) reject resolve:(RCTResponseSenderBlock) resolve) {
     NSError *err = nil;
     if (stream) {
